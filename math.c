4fba498e5dd62168653a280e0bd33e3cc30b832d
Spencer <spencerjcooley07@gmail.com>
2026-05-01 21:53:19 +1000

    Optimised matmul function

--- math.c
+++ math.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include "math.h"
 
+// Multiplies matrices A and B together, result in out.
 void matmul(const Matrix *A, const Matrix *B, Matrix *out) {
     if (A->cols != B->rows) {
-        perror("Matrices have incompatible dimensions\n");
+        perror("Incompatible matrix dimensions.\n");
         return;
     }
 
@@ -13,16 +14,19 @@ void matmul(const Matrix *A, const Matrix *B, Matrix *out) {
     // Inner dimension.
     const int inner = A->cols;
     for (int A_row = 0; A_row < A->rows; A_row++) {
+        // Initialise output row.
+        int out_row = A_row * B->cols;
         for (int B_col = 0; B_col < B->cols; B_col++) {
+            out->data[out_row + B_col] = 0.0f;
+        }
 
-            float sum = 0.0f;
-            // Iterates over both matrices A and B in their row/col.
-            for (int i = 0; i < inner; i++) {
-                sum += A->data[A_row * A->cols + i] * B->data[i * B->cols + B_col];
+        // Iterate over inner dimension.
+        for (int i = 0; i < inner; i++) {
+            float A_val = A->data[A_row * A->cols + i];
+            int B_row = i * B->cols;
+            for (int B_col = 0; B_col < B->cols; B_col++) {
+                out->data[out_row + B_col] += A_val * B->data[B_row + B_col];
             }
-
-            out->data[A_row * inner + B_col];
-
         }
     }
 }
